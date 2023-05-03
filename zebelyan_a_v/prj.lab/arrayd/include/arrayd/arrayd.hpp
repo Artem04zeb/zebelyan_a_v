1632fb4d5a190938081329cfe626269e811a23aa
Artem Zebelyan <artem04zeb@gmail.com>
2023-05-03 11:28:52 +0300

    Update arrayd.hpp

--- zebelyan_a_v/prj.lab/arrayd/include/arrayd/arrayd.hpp
+++ zebelyan_a_v/prj.lab/arrayd/include/arrayd/arrayd.hpp
@@ -1,39 +1,31 @@
 #pragma once
-
 #ifndef ARRAYD_ARRAYD_HPP_20230330
 #define ARRAYD_ARRAYD_HPP_20230330
-
 #include <iosfwd>
 #include <cstdint>
 
-class ArrayD {
 
+class ArrayD {
 public:
 	ArrayD();
 	explicit ArrayD(std::ptrdiff_t ssize);
 	ArrayD(const ArrayD& other);
 	~ArrayD();
-
 	ArrayD& operator=(const ArrayD& rhs);
-	double& operator[](const std::ptrdiff_t i);
-	const double& operator[](const std::ptrdiff_t i) const;
 
-	std::ptrdiff_t ssize() const noexcept;
+	[[nodiscard]] double& operator[](const std::ptrdiff_t i);
+	[[nodiscard]] const double& operator[](const std::ptrdiff_t i) const;
+	[[nodiscard]] std::ptrdiff_t ssize() const noexcept;
 	
 	void insert(const std::ptrdiff_t i, const double value);
-
 	void resize(const std::ptrdiff_t new_size);
-
 	void remove(const std::ptrdiff_t i);
-
 	static const char separator{ ',' };
 
 private:
-	// Указатель на димнамический массив
-	double* coords; 
-
+	double* coords;
 	std::ptrdiff_t size;
-
 	std::ptrdiff_t capacity;
 };
+
 #endif
