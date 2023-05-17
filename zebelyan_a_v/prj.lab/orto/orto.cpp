6b52cc06cabd7055f00eea9394a4ba694026edd3
Artem Zebelyan <artem04zeb@gmail.com>
2023-05-17 11:58:43 +0300

    Update orto.cpp

--- zebelyan_a_v/prj.lab/orto/orto.cpp
+++ zebelyan_a_v/prj.lab/orto/orto.cpp
@@ -1,1 +1,35 @@
 /// ORTO !!
+
+#include <iostream>
+
+class Quad {
+public:
+	Quad(double x_start, double y_start, double l, double h) {
+		this->xs = x_start;
+		this->ys = y_start;
+		this->length = l;
+		this->height = h;
+	}
+	void Draw() {
+		std::cout << std::endl;
+		std::cout << "Start: (" << xs << "," << ys << ")" << std::endl <<
+			"length and height: (" << length << "," << height << ")" << std::endl;
+	}
+
+	void Scale(double k) {
+		length = length * k;
+		height = height * k;
+	}
+
+private:
+	double length, height, xs, ys;
+};
+
+int main() {
+	Quad first(1, 1, -22, 2);
+	first.Draw();
+	first.Scale(2);
+	first.Draw();
+
+	return 0;
+}
