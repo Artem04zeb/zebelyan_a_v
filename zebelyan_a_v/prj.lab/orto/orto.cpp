3f78f0fc0f5a940a856ccc00cb55517064b18828
Artem Zebelyan <artem04zeb@gmail.com>
2023-05-30 01:12:58 +0300

    Update orto.cpp

--- zebelyan_a_v/prj.lab/orto/orto.cpp
+++ zebelyan_a_v/prj.lab/orto/orto.cpp
@@ -1,34 +1,49 @@
-/// ORTO !!
-
 #include <iostream>
 
 class Quad {
 public:
-	Quad(double x_start, double y_start, double l, double h) {
-		this->xs = x_start;
-		this->ys = y_start;
-		this->length = l;
-		this->height = h;
+	Quad(double xs, double ys, double xe, double ye) {
+		this->x_start = xs;
+		this->y_start = ys;
+		this->x_end = xe;
+		this->y_end = ye;
 	}
 	void Draw() {
 		std::cout << std::endl;
-		std::cout << "Start: (" << xs << "," << ys << ")" << std::endl <<
-			"length and height: (" << length << "," << height << ")" << std::endl;
+		std::cout << "Square with vertices:" << std::endl<<
+			"(" << x_start << ", " << y_start << ")" << std::endl <<
+			"(" << x_end << ", " << y_end<< ") " << std::endl;
 	}
 
 	void Scale(double k) {
-		length = length * k;
-		height = height * k;
+		double x_radius = (x_end - x_start) / 2;
+		double y_radius = (y_end - y_start) / 2;
+		double x_center = x_end - (x_end - x_start) / 2;
+		double y_center = y_end - (y_end - y_start) / 2;
+
+		x_start = x_center - x_radius * k;
+		y_start = y_center - y_radius * k;
+		x_end= x_center + x_radius * k;
+		y_end = y_center + y_radius * k;
+
+	}
+	
+	void Union(){
+	
+	}
+
+	void Intersection() {
+
 	}
 
 private:
-	double length, height, xs, ys;
+	double x_start, y_start, x_end, y_end;
 };
 
 int main() {
-	Quad first(1, 1, -22, 2);
+	Quad first(1, 1, 2, 2);
 	first.Draw();
-	first.Scale(2);
+	first.Scale(3.1);
 	first.Draw();
 
 	return 0;
